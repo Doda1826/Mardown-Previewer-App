0b686071da2f05694785cadf4de2b25c6cb92f95
pikachububble <darwinjverasteguis2002@gmail.com>
2023-11-21 15:37:52 -0500

    issue resolved and markdown working

--- src/components/MarkdownPreview.js
+++ src/components/MarkdownPreview.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import marked from 'marked'; 
+import {marked} from 'marked'; 
 
 const MarkdownPreview = () => {
   const markdown = useSelector((state) => state.markdown);
