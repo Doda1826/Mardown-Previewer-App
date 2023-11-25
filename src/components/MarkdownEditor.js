a1b0c6989fd5045bb22e5ad7e735fa7f9b2c1db6
pikachububble <darwinjverasteguis2002@gmail.com>
2023-11-25 17:55:51 -0500

    added default content on the previewer

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { updateMarkdown} from "../redux/actions";
 import '../style.css'
@@ -6,6 +6,11 @@ import '../style.css'
 const MarkdownEditor = () => {
     const dispatch = useDispatch();
     const markdown = useSelector((state) => state.markdown);
+
+    //set default content on component mount
+    useEffect(() => {
+        dispatch(updateMarkdown(markdown))
+    }, [dispatch, markdown]); 
 
     const handleInputChange = (e) => {
         dispatch(updateMarkdown(e.target.value));
